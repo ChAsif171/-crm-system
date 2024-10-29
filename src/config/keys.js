e17f49a59c22d5ae2098eaa0f267ca6cd5e79419
Choudhary Asif <109628034+ChAsif171@users.noreply.github.com>
2024-10-29 13:34:07 +0500

    Create keys.js

---
+++ src/config/keys.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+export default {
+    PORT: process.env.PORT,
+    DATABASE: {
+        URL: process.env.MONGODB_URL,
+    },
+    JWT_SECRET: process.env.JWT_SECRET,
+    REDIS: {
+        REDIS_PASSWORD: process.env.REDIS_CLOUD_PASSWORD,
+        REDIS_HOST: process.env.REDIS_CLOUD_HOST,
+        REDIS_PORT: process.env.REDIS_CLOUD_PORT,
+    },
+    SENDGRID: {
+        API_SECRET: process.env.SENDGRID_API_KEY,
+        FROM: process.env.SENDGRID_FROM_EMAIL,
+    },
+    TWILIO: {
+        ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
+        AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
+        FROM_NUMBER: process.env.TWILIO_FROM_NUMBER,
+    },
+};
