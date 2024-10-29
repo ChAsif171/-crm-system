6f5f70633469bad9f8336eeb6e374d12a4372b7e
Choudhary Asif <109628034+ChAsif171@users.noreply.github.com>
2024-10-29 13:34:46 +0500

    Create index.js

---
+++ src/controllers/auth/index.js
@@ -0,0 +1,13 @@
+import signup from './signup.js';
+import login from './login.js';
+import verifyEmail from './verifyEmail.js';
+import verifyPhoneNumber from './verifyPhoneNumber.js';
+import verifySignin from './verifySignin.js';
+
+export {
+    signup,
+    verifyEmail,
+    verifyPhoneNumber,
+    login,
+    verifySignin,
+}
