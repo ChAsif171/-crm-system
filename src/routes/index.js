04408ed48ade78559cba320ebd244d77a2175706
Choudhary Asif <109628034+ChAsif171@users.noreply.github.com>
2024-10-29 19:03:46 +0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
-import user from './user.js'
+import user from './user.js';
+import payment from './payment.js';
+import role from './roles.js';
+import permissions from './permissions.js';
+import AdminSignin from '../models/adminSignin.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 
 router.use('/users', user);
+router.use('/payment', payment);
+router.use('/roles', authMiddleware(AdminSignin), role);
+router.use('/permissions', authMiddleware(AdminSignin), permissions);
 
 export default router;
