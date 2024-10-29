c3292b07a431aa11262290578cdc3269c37c1ecf
Choudhary Asif <109628034+ChAsif171@users.noreply.github.com>
2024-10-29 19:03:06 +0500

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -56,7 +56,20 @@ const ExpirySeconds = Object.freeze({
     h12: 43200,
     d1: 86400,
 });
-
+const Permissions = Object.freeze({
+    getAdminAccounts: "650c45b07812ee6317f261b4",
+    updateAdminRole: "650c4d897812ee6317f261c8",
+    payment: "650ab361362f806c5391dced", // manualAdjustment and manualFee
+    createUser: "650ab379362f806c5391dcf7",
+    getRole: "650ab37e362f806c5391dcf9",
+    editRole: "650ab384362f806c5391dcfb",
+    createRole: "650ab389362f806c5391dcfd",
+    deleteRole: "650ab38d362f806c5391dcff",
+    getPermessions: "650ab391362f806c5391dd01",
+    createAdmin: "6512a982dba2d25ba5f10103",
+    allFeeDetails: "6512af11dba2d25ba5f10104",
+    deleteAdmin: "6512b115975bb75fa54f3c8a",
+});
 
 export {
     customLevels,
@@ -65,4 +78,5 @@ export {
     OtpTypes,
     RedisConnectionObject,
     ExpirySeconds,
+    Permissions,
 }
