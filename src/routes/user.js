2ab8db6d7c53197765d10398a1e342dd8397f1c1
Choudhary Asif <109628034+ChAsif171@users.noreply.github.com>
2024-10-29 13:36:40 +0500

    Create user.js

---
+++ src/routes/user.js
@@ -0,0 +1,18 @@
+import express from "express";
+import User from '../models/users.js';
+import authMiddleware from '../middleware/authMiddleware.js';
+import validateUserInputs from '../middleware/validateUserInputs.js';
+import VerifyOtpMiddleware from '../middleware/verifyOtpMiddleWare.js';
+import { EmailSchema, OtpSchema, PasswordConfirmPasswordSchema, UserSignupSchema, SignInSchema, PhoneSchema } from '../../constants/usersJoiSchemas.js';
+import { signup, verifyEmail, verifyPhoneNumber, login, verifySignin } from '../controllers/auth/index.js';
+
+const router = express.Router();
+
+
+router.post('/signup', validateUserInputs(UserSignupSchema), signup);
+router.post('/verify-email', validateUserInputs(OtpSchema), authMiddleware(User), VerifyOtpMiddleware, verifyEmail);
+router.post('/verify-phonenumber', validateUserInputs(PhoneSchema), authMiddleware(User), VerifyOtpMiddleware, verifyPhoneNumber);
+router.post('/login', validateUserInputs(SignInSchema), login);
+router.post('/verify-signin', validateUserInputs(EmailSchema), authMiddleware(User), VerifyOtpMiddleware, verifySignin);
+
+export default router;
